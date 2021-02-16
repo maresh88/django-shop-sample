4e58d16515345de20ed0ab7f3276bd0e60dbfc52
Artem Moriashov <maresh88@mail.ru>
2021-02-16 15:37:05 +0300

    setting up the homepage adding filter by category

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='homepage'),
+    path('category/<slug:category_slug>/', views.ProductsByCategory.as_view(), name='products_by_category'),
     path('detail/<int:pk>/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('', views.HomePageView.as_view(), name='homepage'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -12,6 +12,7 @@ class AvailabilityMixin:
 class HomePageView(AvailabilityMixin, ListView):
     queryset = Product.objects.all()
     context_object_name = 'products'
+    template_name = 'shop/homepage.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
@@ -23,3 +24,10 @@ class ProductDetailView(AvailabilityMixin, DetailView):
     model = Product
     template_name = 'shop/product_detail.html'
     context_object_name = 'product'
+
+
+class ProductsByCategory(HomePageView):
+    template_name = 'shop/product_list.html'
+
+    def get_queryset(self):
+        return super(ProductsByCategory, self).get_queryset().filter(category__slug=self.kwargs.get('category_slug'))
