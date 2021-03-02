75924a94eac2d730a8db0c1132ad2e90ca6b818d
ArtemMoriashov <maresh88@mail.ru>
2021-03-02 17:07:35 +0300

    adding ajax request to add products to the cart on the product_detail.html displaying product quantity on the navbar.html

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-
+    path('add/<int:product_id>/', views.CartAdd.as_view(), name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic.base import View
+from django.http import JsonResponse
+from .cart import Cart
+from shop.models import Product
 
-# Create your views here.
+
+class CartAdd(View):
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+        return JsonResponse({'added': 'OK'})
